f2819b31db219ae1791ed0cda5c594b38e4727cb
nuggetgtrjr <samuel.norris@outlook.com.au>
2026-04-17 13:46:11 +1000

    e

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody3D
 
 
-const SPEED = 5.0
+var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var mouse_sensitivity = 0.002
 
@@ -23,6 +23,12 @@ func _physics_process(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		
+	if Input.is_action_pressed("Player_run"):
+		SPEED = 10.0
+	else:
+		SPEED = 5.0
+		
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
