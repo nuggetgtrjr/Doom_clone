8c77768a0540d2cdfc6b7ba1578f75a54473988a
nuggetgtrjr <samuel.norris@outlook.com.au>
2026-04-30 14:41:00 +1000

    shooting and fixes

--- Scenes/player.gd
+++ Scenes/player.gd
@@ -4,10 +4,17 @@ extends CharacterBody3D
 var SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 var mouse_sensitivity = 0.002
+var bulletscene = preload("res://Scenes/bullet.tscn")
+var bulletspawn
+var ammo : int = 5
+var player_health = 100
+
+
 
 
 func _ready():
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	bulletspawn = get_node("Camera3D/bulletspawn")
 
 func _input(event):
 	if event is InputEventMouseMotion and Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
@@ -19,7 +26,6 @@ func _physics_process(delta: float) -> void:
 	# Add the gravity.
 	if not is_on_floor():
 		velocity += get_gravity() * delta
-
 	# Handle jump.
 	if Input.is_action_just_pressed("player_jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
@@ -28,8 +34,9 @@ func _physics_process(delta: float) -> void:
 		SPEED = 10.0
 	else:
 		SPEED = 5.0
-		
-
+	if Input.is_action_just_pressed("player_shoot"):
+		shoot()
+	
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir := Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
@@ -42,3 +49,12 @@ func _physics_process(delta: float) -> void:
 		velocity.z = move_toward(velocity.z, 0, SPEED)
 
 	move_and_slide()
+
+func shoot():
+	#handles shooting
+	var bullet = bulletscene.instantiate()
+	get_parent().add_child(bullet)
+	bullet.global_transform = bulletspawn.global_transform
+	bullet.scale= Vector3(0.1, 0.1, 0.1)
+	ammo -= 1
+	
